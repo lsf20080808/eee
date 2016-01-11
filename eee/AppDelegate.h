d86640061d0f25c3cf7129625c3a370ad4fd8faf
App <App@shangfeidemacbook-pro.local>
2016-01-11 23:43:37 +0800

    Initial Commit

---
+++ eee/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  eee
+//
+//  Created by App on 16/1/11.
+//  Copyright © 2016年 liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
